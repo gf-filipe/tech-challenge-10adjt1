0067129d4b1e073522bf66470b83b2da334d2572
Alex Sousa <alexsousaalves@gmail.com>
2025-11-01 16:24:16 -0300

    chore: update login

--- src/main/java/br/com/fiap/techchallenge/controllers/AuthenticationController.java
+++ src/main/java/br/com/fiap/techchallenge/controllers/AuthenticationController.java
@@ -11,10 +11,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,12 +39,20 @@ public class AuthenticationController {
             @ApiResponse(responseCode = "401", description = "Credenciais inválidas", content = @Content),
             @ApiResponse(responseCode = "400", description = "Dados de login inválidos", content = @Content)
     })
+
     @PostMapping("/v1/login")
-    public ResponseEntity<TokenDTO> login(@RequestBody @Valid LoginDTO dados) {
-        var authToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
-        var authentication = manager.authenticate(authToken);
-        var tokenJWT = tokenService.gerarToken((UserDetails) authentication.getPrincipal());
-        return ResponseEntity.ok(new TokenDTO(tokenJWT));
+    public ResponseEntity<?> login(@RequestBody @Valid LoginDTO dados) {
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+            var authentication = manager.authenticate(authToken);
+            var tokenJWT = tokenService.gerarToken((UserDetails) authentication.getPrincipal());
+            return ResponseEntity.ok(new TokenDTO(tokenJWT));
+        } catch (AuthenticationException e) {
+            System.err.println("Falha na autenticação: " + e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Credenciais inválidas: e-mail ou senha incorretos.");
+        }
     }
 
 }
